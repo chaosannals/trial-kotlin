8e0e639a7ed624a896d1be7c504f112f18a4e64f
chaosannals <chaosannals@outlook.com>
2022-09-28 18:00:08 +0800

    save

---
+++ cmddemo/app/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.7.0"
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    val ktor_version = "2.1.1"
+
+    // Align versions of all Kotlin components
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+
+    // Use the Kotlin JDK 8 standard library.
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("com.google.guava:guava:30.1.1-jre")
+
+    // ktor
+    implementation("io.ktor:ktor-client-core:$ktor_version")
+    implementation("io.ktor:ktor-client-cio:$ktor_version")
+
+    // Use the Kotlin test library.
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
+
+application {
+    // Define the main class for the application.
+    mainClass.set("trial.kotlin.cmddemo.AppKt")
+}
